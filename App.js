8bb15ac5e366cbc6a0c7e6cce01e50f62e445688
Alfaya <fcalfaya@gmail.com>
2019-11-11 00:14:22 -0300

    criando enviando aruivo APP e index

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/App.css';
+import Page from './pages/page'
+
+function App() {
+  return (
+    <div>
+      <Page/>
+    </div>
+  );
+}
+
+export default App;
